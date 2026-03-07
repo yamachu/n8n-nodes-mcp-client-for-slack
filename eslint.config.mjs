711a60a259a3066013c48391805c6ac45a7ffe49
yamachu <yamachu.dev@gmail.com>
2026-03-08 00:03:32 +0900

    n8n Cloudのサポートはしないので、n8nのランタイム依存のパッケージに依存させる

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,10 @@
 import { config } from '@n8n/node-cli/eslint';
 
-export default config;
+export default [
+	...config,
+	{
+		rules: {
+			'@n8n/community-nodes/no-restricted-imports': 'off',
+		},
+	},
+];
